ea94f68f178f71aedad8c90fd62c726280d9680f
mateo-covacho_globant <mateo.covacho@globant.com>
2023-06-19 16:47:36 +0200

    tsigonre

--- pages/api/users/usertable_exists.ts
+++ pages/api/users/usertable_exists.ts
@@ -7,24 +7,40 @@ type Data = {
   error?: string;
 };
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
   print("blue", "/api/users/usertable_exists.ts");
   print("yellow", "\t Checking usertable ... ");
-
-  const { NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY }: { NEXT_PUBLIC_SUPABASE_URL: string; NEXT_PUBLIC_SUPABASE_ANON_KEY: string } =
-    process.env;
+  // @ts-ignore
+  const {
+    NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  }: {
+    NEXT_PUBLIC_SUPABASE_URL: string;
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+  } = process.env;
   print("yellow", "\t\t creating client ...");
-  const supabase = createClient(NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY);
+  const supabase = createClient(
+    NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
   print("green", "\t\t client created");
 
   const { uuid } = req.query;
   print("yellow", `\t\t checking uuid: ${uuid} for user table ...`);
-  const { data, error } = await supabase.from("users").select("id").eq("id", uuid);
+  const { data, error } = await supabase
+    .from("users")
+    .select("id")
+    .eq("id", uuid);
 
   let tableExists = false;
   if (error) {
     print("red", "\t" + error);
-    return res.status(500).json({ error: "An error occurred while checking the user table." });
+    return res
+      .status(500)
+      .json({ error: "An error occurred while checking the user table." });
   } else if (data.length === 0) {
     tableExists = false;
   } else {
