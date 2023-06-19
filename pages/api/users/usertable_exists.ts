263d89dd47d29601acf5cd20fd094024a057be56
mateo-covacho_globant <mateo.covacho@globant.com>
2023-06-19 15:53:23 +0200

    tsignore

--- pages/api/users/usertable_exists.ts
+++ pages/api/users/usertable_exists.ts
@@ -5,23 +5,35 @@ import { differenceInSeconds } from "date-fns";
 import { table } from "console";
 
 import { print } from "../../../utils/print";
+import ts from "typescript";
 
 type Data = {
   name: string;
 };
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
   print("blue", "/api/users/usertable_exists.ts");
   print("yellow", "\t Checking usertable ... ");
 
   print("yellow", "\t\t creating client ...");
-  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string);
+
+  // @ts-ignore
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+  );
   print("green", "\t\t client created");
 
   const { uuid } = req.query;
   //	@ts-ignore
   print("yellow", `\t\t checking uuid: ${uuid} for user table ...`);
-  const { data, error } = await supabase.from("users").select("id").eq("id", uuid);
+  const { data, error } = await supabase
+    .from("users")
+    .select("id")
+    .eq("id", uuid);
 
   let table_exists = false;
   //	@ts-ignore
